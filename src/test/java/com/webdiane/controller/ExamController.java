fbdc8b3a8c27c94872e8ecdb8bad23e00301235d
DianeChoi14 <miseol914@gmail.com>
2024-07-04 17:14:45 +0900

    feat : ProductVO 잘 수집되었다면 RedirectAttributes 객체에 msg값 바인딩

--- src/test/java/com/webdiane/controller/ExamController.java
+++ src/test/java/com/webdiane/controller/ExamController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.webdiane.model.ProductVO;
 
@@ -29,7 +30,10 @@ public class ExamController
    {
 	   // '/WEB-INF/views/exam/inputProduct.jsp'
    }
+   
+   
    /*
+   //@RequestParam을 이용한 매개변수 수집 방법
    @RequestMapping(value="/inputProduct", method=RequestMethod.POST)
    public void saveProduct
    	(
@@ -48,11 +52,21 @@ public class ExamController
 	   System.out.println(p.toString());
    }
    */
+   // 파라미터를 Spring framework가 자동수집하여 객체로 만들어준다.
    @RequestMapping(value="/inputProduct", method=RequestMethod.POST)
-   public void saveProduct(ProductVO p) 
+   public String saveProduct(ProductVO p, RedirectAttributes rediraAttributes) 
    {
 	   System.out.println("saveProduct()호출됨");
 //	   ProductVO p = new ProductVO(productNo, productName, qty, price, productNickName);
 	   System.out.println(p.toString());
+	   String msg = null;
+	   if(p!=null)
+	   {
+		   msg = "success";
+	   }
+	   
+	   rediraAttributes.addAttribute("status", msg); // 페이지가 넘어갈 때 바인딩된 객체를 들고 간다
+	   
+	   return "redirect:/";
    }
 }
