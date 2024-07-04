989fc1e571ce0d91a1170359a13ae4ffa49ed45a
DianeChoi14 <miseol914@gmail.com>
2024-07-04 16:36:21 +0900

    feat:examController 수정

---
+++ src/test/java/com/webdiane/controller/ExamController.java
@@ -0,0 +1,58 @@
+package com.webdiane.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.webdiane.model.ProductVO;
+
+@Controller
+@RequestMapping("/exam") // "/exam" uri로 호출되면 현재의 컨트롤러가 동작하도록 매핑
+public class ExamController 
+{
+   @RequestMapping("/outputProduct") // "/exam/outputProduct" uri로 호출되면 동작하도록 매핑
+   public void outputProduct(Model model)
+   {
+      System.out.println("outputProduct().......");
+      ProductVO exproduct = new ProductVO("240704_1", "새우깡", 5, 2000, "손이가요 손이가~");
+      System.out.println(exproduct.toString());
+      
+      model.addAttribute("product", exproduct); // 바인딩 한 것. 어디에 담아야 하나? model
+      
+      // '/WEB-INF/views/exam/outputProduct.jsp'
+   }
+   
+   @RequestMapping("inputProduct") // '/exam/inputProduct'가 get방식으로 호출되었을 때, 아래의 view페이지를 출력한다.
+   public void inputProduct()
+   {
+	   // '/WEB-INF/views/exam/inputProduct.jsp'
+   }
+   /*
+   @RequestMapping(value="/inputProduct", method=RequestMethod.POST)
+   public void saveProduct
+   	(
+   			@RequestParam("productNo") String productNo, 
+   			@RequestParam("productName") String productName,  
+   			@RequestParam("qty") int qty,  
+   			@RequestParam("price") int price,  
+   			@RequestParam("productNickName") String productNickName
+   			
+   	)
+   {
+	   // '/exam/inputProduct'가 post방식으로 호출될 때 현재 메서드가 맵핑되도록   
+	   System.out.println("saveProduct()호출됨");
+	   System.out.println("제품번호 : " + productNo);
+	   ProductVO p = new ProductVO(productNo, productName, qty, price, productNickName);
+	   System.out.println(p.toString());
+   }
+   */
+   @RequestMapping(value="/inputProduct", method=RequestMethod.POST)
+   public void saveProduct(ProductVO p) 
+   {
+	   System.out.println("saveProduct()호출됨");
+//	   ProductVO p = new ProductVO(productNo, productName, qty, price, productNickName);
+	   System.out.println(p.toString());
+   }
+}
